c40db39c4b2c651c0f9dcd96a73293d32af8bc04
tars003 <sharmaajay2kuu@gmail.com>
2022-11-05 01:34:26 +0530

    first commit

---
+++ models/SerialResponse.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const SerialResponse = mongoose.Schema({
+    mcId: String,
+    data: String,
+    time: String,
+    date: String,
+    temp1: String,
+    temp2: String,
+    temp3: String,
+    temp4: String,
+    temp5: String,
+    ph1: String,
+    ph2: String,
+    ph3: String,
+    ph4: String,
+    ph5: String,
+    rh: String,
+    energy: String,
+    flow: String,
+    flowVolume: String
+});
+
+module.exports = mongoose.model("serialResponse", SerialResponse);
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const moment = require('moment');
+const path = require('path');
+const axios = require('axios');
+var request = require('requests');
+
+const connectDB = require('./utils/db');
+
+const SerialResponse = require('./models/SerialResponse.model');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+require('dotenv').config();
+
+
+connectDB();
+
+
+// TEST ROUTE
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+// GET DATA FROM MICROCOTROLLER
+app.post('/serial-data', async (req, res) => {
+    try {
+        let { 
+            mcId, 
+            time, 
+            date, 
+            temp1, 
+            temp2, 
+            temp3, 
+            temp4, 
+            temp5,
+            ph1, 
+            ph2, 
+            ph3, 
+            ph4, 
+            ph5,
+            rh,
+            energy,
+            flow,
+            flowVolume
+        } = req.body;
+
+        console.log('Serial Reponse : ', req.body);
+
+
+        const serialRes = await SerialResponse.create(req.body);
+
+        return res.status(200).json({
+            success: true,
+            data: serialRes
+        });
+
+    } catch (err) {
+        console.log('error !', err);
+        return res.status(503).json({
+            success: false,
+            error: err
+        })
+    }
+});
+
+// GET SPECIFIC MICROCONTROLLER'S ALL SERIAL REPONSES
+app.get('/get-data/:mcId', async (req, res) => {
+    try {
+        const srObj = await SerialResponse.find({mcId: `${req.params.mcId}`});
+        return res.status(200).json({
+            success: true,
+            count: srObj.length,
+            data: srObj
+        });
+    } catch (err) {
+        console.log('error !', err);
+        return res.status(503).json({
+            success: false,
+            error: err
+        })
+    }
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    let port = process.env.PORT || 3000;
+    console.log(`listening on localhost:${port}`);
+});
